f325eba1d79be3ab1f7b17ff7002336216909be5
karies2016 <karandasov.e@mail.ru>
2016-11-19 23:12:42 +0300

    добавление конфигов gunicorn и коррекция конфигов nginx

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default       
 sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
+sudo gunicorn -b 0.0.0.0:8080 hello:app &
