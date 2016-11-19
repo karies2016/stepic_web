0887e9cc26a7f5fcb66bd7c9dc49f50b75947ffd
karies2016 <karandasov.e@mail.ru>
2016-11-19 23:44:45 +0300

    корректировка конфигов

--- hello.py
+++ hello.py
@@ -2,4 +2,4 @@ def app(environ, start_response):
     qs = environ['QUERY_STRING']
     ls = qs.split("&")
     start_response('200 OK', [('Content-Type', 'text/plain')])
-return [ "\n".join(ls) ]
+    return [ "\n".join(ls) ]
