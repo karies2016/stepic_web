6a54997d4fd2cd7c04605d2a345730c674562ab3
karies2016 <karandasov.e@mail.ru>
2016-11-20 15:43:08 +0300

    Поправил конфиги для gunicorn

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default       
 sudo /etc/init.d/nginx restart
 source web/ask/webvenv/bin/activate
-gunicorn -b 0.0.0.0:8080 /usr/bin/python3 /home/box/web/ hello:app &     
-gunicorn -b 0.0.0.0:8000 /usr/bin/python3 /home/box/web/ask ask.wsgi:application &
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+sudo /etc/init.d/gunicorn restart
